0667cadb676b09fd346f15bffa12b28aa95ca1fb
chomosuke <a13323600@gmail.com>
2022-04-17 18:21:21 +1000

    allowed assertion in jest

--- jest.js
+++ jest.js
@@ -1,6 +1,18 @@
+const base = require('./index');
+
 module.exports = {
     extends:  [
         'plugin:jest/recommended',
         'plugin:jest/style',
     ],
+    rules: {
+        'no-restricted-syntax': [
+            ...base.rules['no-restricted-syntax'].filter(
+                (e) => (
+                    e.selector !== 'TSTypeAssertion' &&
+                    e.selector !== 'TSAsExpression'
+                ),
+            ),
+        ],
+    },
 };
